d2e7a7d732070522e36b94f55b8e0568f6cdd81d
Andrea <andiesilvamachado@gmail.com>
2019-04-15 21:31:43 -0500

    mi primer calculadora

---
+++ index.js
@@ -0,0 +1,23 @@
+
+function sum(num1, num2) {
+return num1 + num2;
+
+}
+function rest(num1, num2) {
+return num1 - num2;
+}
+
+
+
+function div(num1, num2) {
+return num1 / num2;
+}
+
+function mult(num1, num2) {
+return num1 * num2;
+}
+
+window.calculator = {sum, rest, div, mult};
+
+
+
